e9eddd3a9b4a0ac944ed88ca6b3defdb6520e088
Nathan Gallo <nathanmgallo@gmail.com>
2021-04-26 09:48:23 -0400

    made a rally object

---
+++ server/repos/RallyObject.js
@@ -0,0 +1,10 @@
+class RallyObject {
+    constructor(name, notes, description, c_EmergingTechPOCPipeline){
+        this.Name = name;
+        this.Notes = notes;
+        this.Description = description;
+        this.c_EmergingTechPOCPipeline = c_EmergingTechPOCPipeline;
+    }
+}
+
+module.exports = RallyObject;
