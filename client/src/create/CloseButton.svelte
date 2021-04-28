2f6cb87ab339688b91bd4b00f1d20c7c05e3c5b9
Gallo, Nathan M (HIMCO, FIN) <nathan.gallo@thehartford.com>
2021-04-28 14:25:01 -0400

    modal created

---
+++ client/src/create/CloseButton.svelte
@@ -0,0 +1,16 @@
+<script>
+    // This property is used by Modal.svelte to pass down the close function
+    export let onClose;
+  </script>
+  
+  <style>
+    /* Customize to your liking */
+    button {
+      position: absolute;
+      top: -3rem;
+      right: 0;
+    }
+  </style>
+  
+  <button on:click={onClose}>Custom Close Button</button>
+  
